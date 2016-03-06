78cdaea69b4f43a9a090cd73b11a9c5cf54d63b2
Felipe Baytelman <felipe.baytelman@gmail.com>
2016-03-06 01:01:47 -0800

    Da password

--- config/config-travis.js
+++ config/config-travis.js
@@ -4,8 +4,7 @@ config.api = {};
 config.api.port = 3000;
 
 config.db = {};
-config.db.url = "postgresql://postgres:" +
-    process.env.POSTGRES_PASSWORD + "@" + process.env.POSTGRES_PORT_5432_TCP_ADDR +
+config.db.url = "postgresql://postgres:ourlittlesecret@" + process.env.POSTGRES_PORT_5432_TCP_ADDR +
     ":5432/ratings_test?param=value";
 
 console.log(process.env.POSTGRES_PORT_5432_TCP_ADDR +
