c9b4e2d88e4ac46666a377df44f0cfc3e2e0e216
Felipe Baytelman <felipe.baytelman@gmail.com>
2016-03-06 02:44:20 -0800

    URL for heroku

--- config/config-prod.js
+++ config/config-prod.js
@@ -4,10 +4,6 @@ config.api = {};
 config.api.port = 80;
 
 config.db = {};
-config.db.url = "postgresql://postgres:ourlittlesecret@" + process.env.POSTGRES_PORT_5432_TCP_ADDR +
-    ":5432/ratings_test?param=value";
-
-console.log(process.env.POSTGRES_PORT_5432_TCP_ADDR +
-":5432/ratings_test?param=value")
+config.db.url = process.env.PROD_DB_URL;
 
 module.exports = config;
