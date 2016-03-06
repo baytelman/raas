06e98b8b4efd105b20654304c951ada6f6477bda
Felipe Baytelman <felipe.baytelman@gmail.com>
2016-03-06 02:53:28 -0800

    Print config

--- config/config-prod.js
+++ config/config-prod.js
@@ -6,6 +6,4 @@ config.api.port = 80;
 config.db = {};
 config.db.url = process.env.PROD_DB_URL;
 
-console.log(config.db.url);
-
 module.exports = config;
--- src/config.js
+++ src/config.js
@@ -2,4 +2,11 @@ var env = process.env.ENV;
 
 if (!env) throw new Error("Missing ENV variable");
 
-module.exports = require('../config/config-' + env + ".js");
+config = require('../config/config-' + env + ".js");
+
+console.log("###############################");
+console.log("config:");
+console.log(config);
+console.log("###############################");
+
+module.exports = config;
